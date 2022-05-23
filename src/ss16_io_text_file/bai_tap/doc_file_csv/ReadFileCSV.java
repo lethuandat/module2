e439ca67d8b80f5f8c8cce3b98f56049c8cda13e
lethuandat <levanle5@gmail.com>
2022-05-23 08:05:11 +0700

    update bai tap

---
+++ src/ss16_io_text_file/bai_tap/doc_file_csv/ReadFileCSV.java
@@ -0,0 +1,4 @@
+package ss16_io_text_file.bai_tap.doc_file_csv;
+
+public class ReadFileCSV {
+}
---
+++ src/ss16_io_text_file/thuc_hanh/tim_gia_tri_max_in_ra_file/FindMaxValue.java
@@ -0,0 +1,22 @@
+package ss16_io_text_file.thuc_hanh.tim_gia_tri_max_in_ra_file;
+
+import java.util.List;
+
+public class FindMaxValue {
+    public static int findMax(List<Integer> numbers) {
+        int max = numbers.get(0);
+        for (Integer number : numbers) {
+            if (max < number) {
+                max = number;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        ReadAndWriteFile readAndWriteFile = new ReadAndWriteFile();
+        List<Integer> numbers = readAndWriteFile.readFile("numbers.txt");
+        int maxValue = findMax(numbers);
+        readAndWriteFile.writeFile("result.txt", maxValue);
+    }
+}
