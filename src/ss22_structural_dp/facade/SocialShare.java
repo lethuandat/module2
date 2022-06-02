2d2e62927adb97f097ab56e615e06e4ca772ad05
lethuandat <levanle5@gmail.com>
2022-06-02 10:02:43 +0700

    up thuc hanh ss23

---
+++ src/ss22_structural_dp/facade/SocialShare.java
@@ -0,0 +1,7 @@
+package ss22_structural_dp.facade;
+
+public interface SocialShare {
+    void setMessage(String message);
+
+    void share();
+}
---
+++ src/ss23_behavioral_dp/strategy/SortStrategy.java
@@ -0,0 +1,7 @@
+package ss23_behavioral_dp.strategy;
+
+import java.util.List;
+
+public interface SortStrategy {
+    <T> void sort(List<T> items);
+}
