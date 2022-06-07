ddefa2da383d42220c474b13cf839594b788626e
lethuandat <98041731+lethuandat@users.noreply.github.com>
2022-06-07 15:17:23 +0700

    Update PersonServiceImpl.java

--- src/bai_tap_them/quan_ly_truong_hoc/service/PersonServiceImpl.java
+++ src/bai_tap_them/quan_ly_truong_hoc/service/PersonServiceImpl.java
@@ -20,7 +20,7 @@ import java.util.Scanner;
 public class PersonServiceImpl implements IService {
     public static final String REGEX_STUDENT_ID = "\\d{8}";
     public static final String REGEX_TIME = "^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[13-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$";
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy");
 
     List<Student> studentList = new ArrayList<>();
     List<Teacher> teacherList = new ArrayList<>();
